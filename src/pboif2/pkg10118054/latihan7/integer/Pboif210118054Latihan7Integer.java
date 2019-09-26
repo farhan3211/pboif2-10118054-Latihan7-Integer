2f48c07fa6b2819e1d5dd773b8b7bd0cf3daa337
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:20:11 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan7/integer/Pboif210118054Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan7.integer;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     * byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
